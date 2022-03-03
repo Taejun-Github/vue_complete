9def0ec7b07b3653e2105b354b98f031944aa1c0
TAEJUN\ausch <tjkimin2021@gmail.com>
2022-03-03 10:43:03 +0900

    라우터 구현

--- vue-news/src/main.js
+++ vue-news/src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue';
 import App from './App.vue';
+import { router } from './router/index.js';
+
 
 Vue.config.productionTip = false;
 
+
 new Vue({
   render: h => h(App),
+  router, // 원래는 router: router이다.
 }).$mount('#app');
---
+++ vue-news/src/router/index.js
@@ -0,0 +1,43 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+// 왜 바로 vue-router로 가져올 수 있을까? package.json에 정의되어 있기 때문이다.
+import NewsView from '../views/NewsView.vue';
+import AskView from '../views/AskView.vue';
+import JobsView from '../views/JobsView.vue';
+import ItemView from '../views/ItemView.vue';
+import UserView from '../views/UserView.vue';
+
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+    routes: [
+        {
+            path: '/',
+            redirect: '/news',
+            // 처음 접속시 new로 리다이렉트된다.
+        },
+        {
+            // path에는 url에 대한 주소가 들어간다
+            path: '/news',
+            // component에는 url 주소로 갔을 때 표시될 컴포넌트가 표시된다.
+            component: NewsView,
+        },
+        {
+            path: '/ask',
+            component: AskView,
+        },
+        {
+            path: '/jobs',
+            component: JobsView,
+        },
+        {
+            path: '/user',
+            component: UserView,
+        },
+        {
+            path: '/item',
+            component: ItemView,
+        }
+    ]
+});
