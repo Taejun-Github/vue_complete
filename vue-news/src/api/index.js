91c82f59b4a59363ebade3b5718536c27d428ccf
TAEJUN\ausch <tjkimin2021@gmail.com>
2022-03-25 12:11:48 +0900

    async await 수정

--- vue-news/src/api/index.js
+++ vue-news/src/api/index.js
@@ -7,26 +7,53 @@ const config = {
 
 // 2. API 함수들을 정리한다.
 
-function fetchNewsList() {
-    return axios.get(`${config.baseUrl}news/1.json`);
+async function fetchNewsList() {
+    try {
+        const reseponse = await axios.get(`${config.baseUrl}news/1.json`);
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
+async function fetchAskList() {
+    try {
+        return await axios.get(`${config.baseUrl}ask/1.json`);
+    } catch (error) {
+        console.log(error);
+    }
+}
+async function fetchJobsList() {
+    try {
+        return await axios.get(`${config.baseUrl}jobs/1.json`);
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+async function fetchList(pageName) {
+    try {
+        return await axios.get(`${config.baseUrl}${pageName}/1.json`);
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+async function fetchUserInfo(username) {
+    try {
+        return await axios.get(`${config.baseUrl}user/${username}.json`);
+    } catch (error) {
+        console.log(error);
+    }
+
 }
 
-function fetchAskList() {
-    return axios.get(`${config.baseUrl}ask/1.json`)
-}
+async function fetchCommentItem(id) {
+    try {
+        return await axios.get(`${config.baseUrl}item/${id}.json`);
+    } catch (error) {
+        console.log(error);
+    }
 
-function fetchJobsList() {
-    return axios.get(`${config.baseUrl}jobs/1.json`)
-}
-function fetchList(pageName) {
-    return axios.get(`${config.baseUrl}${pageName}/1.json`);
-}
-function fetchUserInfo(username) {
-    return axios.get(`${config.baseUrl}user/${username}.json`);
-}
-
-function fetchCommentItem(id) {
-    return axios.get(`${config.baseUrl}item/${id}.json`);
 }
 
 
